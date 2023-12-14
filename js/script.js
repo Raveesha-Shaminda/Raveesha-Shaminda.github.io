f57516d581c086033e04c03d631cbe7c5872f4db
Raveesha Shaminda <raveeshashaminda8@gmail.com>
2023-12-15 01:13:40 +0530

    app section (card)
    red hat boy game card
    calculator card
    wathaer card

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@ function signIn(){
     alert("This is not set yet");
 }
 
+function redHatGame(){
+    alert("This is not set yet");
+}
+
+function calculator(){
+    alert("This is not set yet");
+}
+
+function wathaer(){
+    alert("This is not set yet");
+}
